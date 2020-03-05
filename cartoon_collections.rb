8e43129fe0d616af6cdf45788e5d35239af6ee5b
Melissa Wise <wise.melissa19@gmail.com>
2020-03-05 17:57:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(name_array)
   # Your code here
+  name_array.each_with_index do |name, num|
+    puts "#{num + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
+  output = []
+  array.collect do |planeteers|
+  output << planeteers.capitalize + "!"
+end
+return output
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  array.any? { |str| str.size > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each do |ele| 
+  if ele == "cheddar" || ele == "gouda" || ele == "camembert" 
+  output = ele
+end
+return output
 end
